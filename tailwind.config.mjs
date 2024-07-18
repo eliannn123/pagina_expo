a67b0df5591e296a79f7abe3738eb0eb83068bdb
eliannn123 <eliann2110@gmail.com>
2024-07-18 00:36:22 -0600

    Gran avance

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors:{
+		'principal' : '#FCF4E7',
+		'secundario' : '#333333',
+		'comp1' : '#C97F58',
+		'comp2' : '#DFC892',
+		'comp3' : '#627967',
+		'blanco' : '#fefeff',
+		'gris1' : '#c5c5c4',
+		'gris2' : '#b4b4b3',
+		'negro' : '#000101',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
